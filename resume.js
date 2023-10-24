84fbf53e719944f4987db0a5f2e462829b7fa7b1
bhx9843 <143867886+bhx9843@users.noreply.github.com>
2023-10-24 22:19:07 +0530

    Day-5-Task

---
+++ resume.js
@@ -0,0 +1,55 @@
+let myResume={
+    "basics": {
+      "name": "Bhavan S ",
+      "email": "bhx9843@gamil.com",
+      "phone": 9876543211,
+      "degree": "B.E",
+      "location": {
+        "address": "34/2 TNagar",
+        "postalCode": 600030,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "work": [
+      {
+        "company": "Food Hub",
+        "position": "Business Executive",
+        "startDate": "2022-11-04",
+        "endDate": "2023-08-15",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Sona College Of Engineering",
+        "department": "CSE",
+        "studyType": "fulltime",
+        "batch start year": 2019,
+        "batch end year": 2023,
+        "gpa": 8.1,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
